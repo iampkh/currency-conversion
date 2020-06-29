2b79f2605597b1112b6fa1801da977ccaef4db4f
iampkh <pkh.brighter@gmail.com>
2020-06-30 00:02:05 +0900

    Currency Conversion UI
    Base skeleton code executable and
    Adding currency conversion app's user-interface files

---
+++ app/src/main/java/com/app/currencyconversion/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.app.currencyconversion
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.currency_input_fragment)
+
+
+    }
+}
